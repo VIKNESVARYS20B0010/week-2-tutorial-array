f1ccdb4095d59dd9c8dad91638cb2faf8a6abc32
VIKNESVARY RAJOO <93852088+VIKNESVARYS20B0010@users.noreply.github.com>
2022-04-02 20:08:11 +0800

    Update exercise1.c

--- exercise1.c
+++ exercise1.c
@@ -10,7 +10,12 @@
 //main function section 
 int main(){
     //write here your program
-
+float currencyWorld[5] = {4.50, 5.55, 3.65, 1.20, 6.56};
+printf("%.2f\n",currencyWorld[0]);
+printf("%.2f\n",currencyWorld[1]);
+printf("%.2f\n",currencyWorld[2]);
+printf("%.2f\n",currencyWorld[3]);
+printf("%.2f\n",currencyWorld[4]);
 
     return 0;
 }
