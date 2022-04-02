402b5b722724e39e90c1a021049f5bdd881f29cb
VIKNESVARY RAJOO <93852088+VIKNESVARYS20B0010@users.noreply.github.com>
2022-04-02 20:46:57 +0800

    Update exercise2.c

--- exercise2.c
+++ exercise2.c
@@ -10,7 +10,8 @@
 //main function section 
 int main(){
     //write here your program
-
+char myName[20] = {"Viknesvary A/P Rajoo"};
+printf (" My Name is %c%c%c%c%c%c%c%c%c%c %c%c%c %c%c%c%c%c%c%c, Bachelor of Information Technology from University Malaysia Kelantan", myName[0],myName[1],myName[2],myName[3],myName[4],myName[5],myName[6],myName[7],myName[8],myName[9],myName[10],myName[11],myName[12],myName[13],myName[14],myName[15],myName[16],myName[17],myName[18],myName[19],myName[20]); 
 
     return 0;
 }
