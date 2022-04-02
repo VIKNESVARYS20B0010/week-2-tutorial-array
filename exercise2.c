8e7002720bc10606d44121df009e97f30a7d4f30
VIKNESVARY RAJOO <93852088+VIKNESVARYS20B0010@users.noreply.github.com>
2022-04-02 20:23:31 +0800

    Update exercise2.c

--- exercise2.c
+++ exercise2.c
@@ -10,6 +10,28 @@
 //main function section 
 int main(){
     //write here your program
+char myName[20] = {"Viknesvary A/P Rajoo"};
+printf (" %c", myName[0]); 
+printf (" %c", myName[1]); 
+printf (" %c", myName[2]); 
+printf (" %c", myName[3]); 
+printf (" %c", myName[4]); 
+printf (" %c", myName[5]);
+printf (" %c", myName[6]); 
+printf (" %c", myName[7]); 
+printf (" %c", myName[8]); 
+printf (" %c", myName[9]); 
+printf (" %c", myName[10]); 
+printf (" %c", myName[11]);
+printf (" %c", myName[12]);
+printf (" %c", myName[13]);
+printf (" %c", myName[14]);
+printf (" %c", myName[15]);
+printf (" %c", myName[16]);
+printf (" %c", myName[17]);
+printf (" %c", myName[18]);
+printf (" %c", myName[19]);
+printf (" %c", myName[20]);
 
 
     return 0;
