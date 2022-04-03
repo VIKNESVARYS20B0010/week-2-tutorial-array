0ebd283d3565d78a62df89fb367ed75c5a7dd824
VIKNESVARY RAJOO <93852088+VIKNESVARYS20B0010@users.noreply.github.com>
2022-04-03 09:41:20 +0800

    Update exercise3.c

--- exercise3.c
+++ exercise3.c
@@ -10,7 +10,12 @@
 //main function section 
 int main(){
     //write here your program
+char myAlphabet[3][3] = {{'h','g','f'},{'c','n','p'},{'o','m','q'}};
 
+printf(" %c %c %c %c %c %c %c %c %c\n", myAlphabet[0][0],myAlphabet[0][1],myAlphabet[0][2],myAlphabet[1][0],myAlphabet[1][1
+],myAlphabet[1][2],myAlphabet[2][0],myAlphabet[2][1],myAlphabet[2][2]);
+
+printf("Character from Row 3 and Column 3 is %c",myAlphabet[2][2]);
 
     return 0;
 }
